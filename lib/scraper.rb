2956ea3b8bdfc21d7fc81058f12ec3a6c1bcdd85
Zoey Chesny <z.chesny@gmail.com>
2020-04-21 16:56:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,17 +4,27 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+<<<<<<< HEAD
     doc = Nokogiri::HTML(open(index_url))
     students = doc.css('div.student-card')
     students.map do |student|
     {name: student.css('h4.student-name').text,
       location: student.css('p.student-location').text,
       profile_url: student.css('a').first['href']}
+=======
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+    students = doc.css('div.student-card')
+    students.map do |student|
+      student_link = student.css('a').first['href']
+      profile_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/#{student_link}"
+      self.scrape_profile_page(profile_url)
+>>>>>>> 9a489f80adfec659c1b86f995504a14e4a5ec59c
     end
   end
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
+<<<<<<< HEAD
     student = {}
     links = doc.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
     links.each do |link|
@@ -31,6 +41,11 @@ class Scraper
     student[:profile_quote] = doc.css('div.profile-quote').text
     student[:bio] = doc.css('div.description-holder p').text
     student
+=======
+    {name: doc.css('h1.profile-name').text,
+    location: doc.css('h2.profile-location').text,
+    profile_url: profile_url}
+>>>>>>> 9a489f80adfec659c1b86f995504a14e4a5ec59c
   end
 
 end
